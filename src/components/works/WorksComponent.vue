b6459bc3e60be56d4497f5be58f0ef69724d0614
Mykola <mykola.mud@gmail.com>
2023-11-10 09:04:15 +0200

    CDP: The background color has been changed

--- src/components/works/WorksComponent.vue
+++ src/components/works/WorksComponent.vue
@@ -4,7 +4,7 @@ import WorksListComponent from '@/components/works/WorksListComponent';
 </script>
 
 <template>
-    <div class=" bg-c103">
+    <div class=" bg-c107">
         <div class="works-box container mx-auto">
             <h2 class=" text-center font-medium text-[4rem] pt-[7.5rem] pb-[6.25rem]">{{ content.works.title }}</h2>
             <div class="works-box__list">
